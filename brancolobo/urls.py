b9cb0ffb899929c32b17ca1a19213651bba8f68a
Lucaslllll <lucascosta12367@gmail.com>
2020-03-03 10:53:35 -0300

    add inicial

--- brancolobo/urls.py
+++ brancolobo/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include('principal.urls'))
+    path('dashboard/', include('principal.urls')),
+    path('', include('inicial.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
